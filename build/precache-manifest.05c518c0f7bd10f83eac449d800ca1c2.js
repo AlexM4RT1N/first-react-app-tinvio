9b5602f3ddd0eccf673340ba61f2101ce7e6c8fc
Martynenko <4lexm4rtin@gmail.com>
2020-10-21 14:02:48 +0300

    check

---
+++ build/precache-manifest.05c518c0f7bd10f83eac449d800ca1c2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d334963b5b4eb82692f2d87dd41308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc02164bb79e0f615992",
+    "url": "/static/css/main.d3390d78.chunk.css"
+  },
+  {
+    "revision": "7ee089060f96cb5b8728",
+    "url": "/static/js/2.7b3a2894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b3a2894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc02164bb79e0f615992",
+    "url": "/static/js/main.45abbe8d.chunk.js"
+  },
+  {
+    "revision": "31ffe14643aeaf802b5a",
+    "url": "/static/js/runtime-main.a8228878.js"
+  },
+  {
+    "revision": "6f84cc41fec645350b5ea8a93f8132fb",
+    "url": "/static/media/2.6f84cc41.png"
+  },
+  {
+    "revision": "22860f6fb9c0abd4bbdba943a674a702",
+    "url": "/static/media/Bens.22860f6f.svg"
+  },
+  {
+    "revision": "547015e6c80171c6cc0f53dadc22508e",
+    "url": "/static/media/Boxes.547015e6.png"
+  },
+  {
+    "revision": "c6910cfb176cd9212dffe14e828e5de4",
+    "url": "/static/media/Fruits.c6910cfb.png"
+  },
+  {
+    "revision": "effb99e90e0780cae87ebbd2314ac770",
+    "url": "/static/media/GoCheken.effb99e9.png"
+  },
+  {
+    "revision": "e8339bdb9c78441fa5392398baab8e91",
+    "url": "/static/media/Home-page_with-shadow.e8339bdb.png"
+  },
+  {
+    "revision": "324e20e9a1a41589d23a185e553e2ac4",
+    "url": "/static/media/Italian.324e20e9.png"
+  },
+  {
+    "revision": "512a1023c1ffce81aed5d4ef79a73ba7",
+    "url": "/static/media/Jolliebean.512a1023.svg"
+  },
+  {
+    "revision": "02b9cb8641b5d45a31147db5e3c1638e",
+    "url": "/static/media/SimplyWrapps.02b9cb86.png"
+  },
+  {
+    "revision": "10210d0efef54c7344a07921b9c0e1da",
+    "url": "/static/media/logo-color.10210d0e.svg"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "60e4b57e8e09e83f99923ae56a686a1f",
+    "url": "/static/media/rubik-v11-latin-700.60e4b57e.woff2"
+  },
+  {
+    "revision": "b648e18b90bcf0a62f9d8af11c52750b",
+    "url": "/static/media/rubik-v11-latin-700.b648e18b.woff"
+  }
+]);
