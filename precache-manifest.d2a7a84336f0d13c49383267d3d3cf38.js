3ddbbe438a6bb009aa1c4e0ce5c0bd775e84ce2a
Martynenko <4lexm4rtin@gmail.com>
2020-11-02 19:12:00 +0200

    build

---
+++ precache-manifest.d2a7a84336f0d13c49383267d3d3cf38.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2db76948af75235343783571ce10166",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6f4a08834f5021af90e",
+    "url": "./static/css/main.15c48b26.chunk.css"
+  },
+  {
+    "revision": "fad82b4f5cec9c705075",
+    "url": "./static/js/2.1f10609f.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "./static/js/2.1f10609f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f4a08834f5021af90e",
+    "url": "./static/js/main.3dfcce94.chunk.js"
+  },
+  {
+    "revision": "62399a0d248410f5d9fc",
+    "url": "./static/js/runtime-main.a8371de7.js"
+  },
+  {
+    "revision": "39604d8da4d577cd53a2faf0fd01fba8",
+    "url": "./static/media/1.39604d8d.png"
+  },
+  {
+    "revision": "6f84cc41fec645350b5ea8a93f8132fb",
+    "url": "./static/media/2.6f84cc41.png"
+  },
+  {
+    "revision": "ee6014b4838c302fc90d44600892a49e",
+    "url": "./static/media/3.ee6014b4.png"
+  },
+  {
+    "revision": "1ae7edde7361d8eaa57d368a74af9d29",
+    "url": "./static/media/Analytics.1ae7edde.png"
+  },
+  {
+    "revision": "0ad02f3ef995ddf7f90db01b27addaea",
+    "url": "./static/media/Before.0ad02f3e.png"
+  },
+  {
+    "revision": "22860f6fb9c0abd4bbdba943a674a702",
+    "url": "./static/media/Bens.22860f6f.svg"
+  },
+  {
+    "revision": "547015e6c80171c6cc0f53dadc22508e",
+    "url": "./static/media/Boxes.547015e6.png"
+  },
+  {
+    "revision": "c6910cfb176cd9212dffe14e828e5de4",
+    "url": "./static/media/Fruits.c6910cfb.png"
+  },
+  {
+    "revision": "effb99e90e0780cae87ebbd2314ac770",
+    "url": "./static/media/GoCheken.effb99e9.png"
+  },
+  {
+    "revision": "59c329a5775a43dabb02850fc475dabd",
+    "url": "./static/media/Hand_Of_Sean_Demo.59c329a5.ttf"
+  },
+  {
+    "revision": "e8339bdb9c78441fa5392398baab8e91",
+    "url": "./static/media/Home-page_with-shadow.e8339bdb.png"
+  },
+  {
+    "revision": "324e20e9a1a41589d23a185e553e2ac4",
+    "url": "./static/media/Italian.324e20e9.png"
+  },
+  {
+    "revision": "512a1023c1ffce81aed5d4ef79a73ba7",
+    "url": "./static/media/Jolliebean.512a1023.svg"
+  },
+  {
+    "revision": "88015d841a8dfda62e3f68f1cf095352",
+    "url": "./static/media/Manrope-Bold.88015d84.woff2"
+  },
+  {
+    "revision": "b919e673175ff940e9cc4a184ae9b479",
+    "url": "./static/media/Manrope-Bold.b919e673.woff"
+  },
+  {
+    "revision": "1b84502ae687fd23dd119a835dcee5c4",
+    "url": "./static/media/Manrope-Medium.1b84502a.woff2"
+  },
+  {
+    "revision": "bd7c3f3238f2bf43e3d6823d78c2d30a",
+    "url": "./static/media/Manrope-Medium.bd7c3f32.woff"
+  },
+  {
+    "revision": "27e49c663adf7116df3209d340b2c532",
+    "url": "./static/media/Manrope-SemiBold.27e49c66.woff2"
+  },
+  {
+    "revision": "b85ee8ea35c4f49026fccf271589c0fb",
+    "url": "./static/media/Manrope-SemiBold.b85ee8ea.woff"
+  },
+  {
+    "revision": "6a9439ab2d280c2ce02d80227430b31d",
+    "url": "./static/media/OperationalCosts.6a9439ab.png"
+  },
+  {
+    "revision": "6ff3d2953204c58d681ac5b98702276a",
+    "url": "./static/media/Orders.6ff3d295.png"
+  },
+  {
+    "revision": "d581b254fc0b8d281099056a33612533",
+    "url": "./static/media/Screen1.d581b254.png"
+  },
+  {
+    "revision": "c285fa916d680a8d5b79988fffc8bf62",
+    "url": "./static/media/Screen2.c285fa91.png"
+  },
+  {
+    "revision": "554b2d52c49998709a6bd3cf3896d64d",
+    "url": "./static/media/Screen3.554b2d52.png"
+  },
+  {
+    "revision": "02b9cb8641b5d45a31147db5e3c1638e",
+    "url": "./static/media/SimplyWrapps.02b9cb86.png"
+  },
+  {
+    "revision": "877b6401250759a748deab0a425b3397",
+    "url": "./static/media/arrow.877b6401.svg"
+  },
+  {
+    "revision": "7cba07f48d37a44cf2c56a8f5796bee1",
+    "url": "./static/media/instagram.7cba07f4.svg"
+  },
+  {
+    "revision": "a3a770cd76fa9264f8b9b091d270b870",
+    "url": "./static/media/linkedin.a3a770cd.svg"
+  },
+  {
+    "revision": "10210d0efef54c7344a07921b9c0e1da",
+    "url": "./static/media/logo-color.10210d0e.svg"
+  },
+  {
+    "revision": "f74b23d4e5823f8b5c0a4c96b466821a",
+    "url": "./static/media/logo-white.f74b23d4.svg"
+  },
+  {
+    "revision": "91ee0f0684798129565a1ed5ba8123b3",
+    "url": "./static/media/tick.91ee0f06.svg"
+  }
+]);
